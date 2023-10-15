40a253a050abd9ec2d97d3cc5f33b4955b48359d
samisams1 <forsamisams@gmail.com>
2023-10-15 11:56:11 +0300

    add build files

---
+++ client/build/precache-manifest.e2a67629b9c5debcdb5dc59043d0fa46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a6a0f8b0c7bf4eb625",
+    "url": "/static/css/main.0250de8f.chunk.css"
+  },
+  {
+    "revision": "83a6a0f8b0c7bf4eb625",
+    "url": "/static/js/main.e7fdf159.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee17d6e13486b77f291f",
+    "url": "/static/css/2.2173eca5.chunk.css"
+  },
+  {
+    "revision": "ee17d6e13486b77f291f",
+    "url": "/static/js/2.2694fde0.chunk.js"
+  },
+  {
+    "revision": "0f1e5ab62da1eddb8dcfab7d8d43a4c4",
+    "url": "/static/media/sams.0f1e5ab6.jpg"
+  },
+  {
+    "revision": "06ad434b1db172cd6cbb37daccd90996",
+    "url": "/index.html"
+  }
+];
